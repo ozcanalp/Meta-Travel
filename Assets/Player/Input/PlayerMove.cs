dcfccd67b499426ea14975ea7c7658077e684daa
efetigli <netigli@hotmail.com>
2022-02-11 13:32:56 +0300

    Change Player Setting
    Now we can use both: new input system and old input system.

--- Assets/Player/Input/PlayerMove.cs
+++ Assets/Player/Input/PlayerMove.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 move;
+    private Vector3 movement;
+    public CharacterController controller;
+    private float walkSpeed = 10f;
+
+    public void OnMoveInput(InputAction.CallbackContext context)
     {
-        
+        move = context.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Movements();
     }
+
+    private void Movements()
+    {
+        movement = new Vector3(move.x, 0f, move.y);
+        controller.Move(movement * walkSpeed * Time.deltaTime);
+
+        if(movement != Vector3.zero)
+        {
+            transform.forward = movement;
+        }
+    }
+    
 }
