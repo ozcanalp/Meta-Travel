29615a35db735d1813b278b5bcd1b1adec2f5ce8
efetigli <netigli@hotmail.com>
2022-02-14 02:25:05 +0300

    Update CameraRaycast Script

--- Assets/Metatravel/Scripts/CameraRaycast.cs
+++ Assets/Metatravel/Scripts/CameraRaycast.cs
@@ -18,12 +18,14 @@ public class CameraRaycast : MonoBehaviour
         if (Physics.Raycast(this.transform.position, this.transform.TransformDirection(Vector3.forward), out raycastHit, raycastLength, layerMask.value))
         {
             crossHair.GetComponent<Image>().color = Color.yellow;
+            crossHair.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
             isHitButton = true;
             whichButton = raycastHit.transform.gameObject;
         }
         else
         {
             crossHair.GetComponent<Image>().color = Color.white;
+            crossHair.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
             isHitButton = false;
         }
     }
