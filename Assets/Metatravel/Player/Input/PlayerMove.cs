c4507af3a8dbcb69e1f3008db6db22540368b60e
efetigli <netigli@hotmail.com>
2022-02-14 02:13:18 +0300

    Update PlayerMove Script

--- Assets/Metatravel/Player/Input/PlayerMove.cs
+++ Assets/Metatravel/Player/Input/PlayerMove.cs
@@ -7,35 +7,32 @@ public class PlayerMove : MonoBehaviour
 {
     private Vector2 move;
     private Vector3 movement;
-    public CharacterController controller;
-    private float walkSpeed = 12f;
+    private float walkSpeed = 6f;
+    private Rigidbody rigidBody;
 
     private void Start() {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     public void OnMoveInput(InputAction.CallbackContext context)
     {
-        move = context.ReadValue<Vector2>();
-        
+        move = context.ReadValue<Vector2>();      
     }
 
     private void FixedUpdate()
     {
+        if (this.GetComponent<PlayerMenu>().isMenuOpen)
+            return;
+
         Movements();
-        AddGravity();
     }
 
     private void Movements()
     {
         //movement = new Vector3(move.x, 0f, move.y);
         movement = transform.right * move.x + transform.forward * move.y;
-        controller.Move(movement * walkSpeed * Time.deltaTime);
-    }
-
-    private void AddGravity()
-    {
-        controller.Move(Physics.gravity * Time.deltaTime);
+        //controller.Move(movement * walkSpeed * Time.deltaTime);
+        rigidBody.MovePosition(transform.position + movement * walkSpeed * Time.deltaTime);
     }
     
 }
