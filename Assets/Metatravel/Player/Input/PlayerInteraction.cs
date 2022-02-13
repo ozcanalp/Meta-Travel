6a78100eed0a826f2790009d1b61cd52178e6f4d
efetigli <netigli@hotmail.com>
2022-02-14 02:11:33 +0300

    Create PlayerInteraction Script

---
+++ Assets/Metatravel/Player/Input/PlayerInteraction.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField] GameObject mainCamera;
+
+
+    public void OnInteractionInput(InputAction.CallbackContext context)
+    { 
+        if (context.performed && mainCamera.GetComponent<CameraRaycast>().isHitButton)
+        {
+
+            if (mainCamera.GetComponent<CameraRaycast>().whichButton.CompareTag("Button1"))
+                Debug.Log("1. button için url yi buraya yapýþtýr.");
+            else if (mainCamera.GetComponent<CameraRaycast>().whichButton.CompareTag("Button2"))
+                Debug.Log("2. button için url yi buraya yapýþtýr.");
+        }
+    }
+}
