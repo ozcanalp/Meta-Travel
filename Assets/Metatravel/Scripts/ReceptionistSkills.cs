54a142a444b7eafd28a1e47798f2a3b5980a75dc
Beytullah Çetin <pasaport_07@hotmail.com>
2022-02-14 15:44:43 +0300

    Updates

--- Assets/Metatravel/Scripts/ReceptionistSkills.cs
+++ Assets/Metatravel/Scripts/ReceptionistSkills.cs
@@ -8,9 +8,7 @@ public class ReceptionistSkills : MonoBehaviour
     private GameObject receptionist1;
     private Quaternion defaultRotation1;
 
-    private float distance2;
-    private GameObject receptionist2;
-    private Quaternion defaultRotation2;
+    [SerializeField] GameObject recep;
 
     private float turnSpeed = 2;
 
@@ -18,8 +16,6 @@ public class ReceptionistSkills : MonoBehaviour
     {
         receptionist1 = GameObject.Find("Receptionist1");
         defaultRotation1 = receptionist1.transform.rotation;
-        receptionist2 = GameObject.Find("Receptionist2");
-        defaultRotation2 = receptionist2.transform.rotation;
     }
 
     private void Update()
@@ -34,18 +30,6 @@ public class ReceptionistSkills : MonoBehaviour
         else
         {
             receptionist1.transform.rotation = Quaternion.Slerp(receptionist1.transform.rotation, defaultRotation1, turnSpeed * Time.deltaTime);
-        }
-
-        //Receptionist#2
-        distance2 = Vector3.Distance(this.transform.position, receptionist2.transform.position);
-
-        if (distance2 <= 5f)
-        {
-            receptionist2.transform.LookAt(this.transform);
-        }
-        else
-        {
-            receptionist2.transform.rotation = Quaternion.Slerp(receptionist2.transform.rotation, defaultRotation2, turnSpeed * Time.deltaTime);
         }
     }
 }
