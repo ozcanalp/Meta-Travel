649bd73bb97aa42acc1bccb64bea0af76e690be2
efetigli <netigli@hotmail.com>
2022-02-16 21:26:56 +0300

    Update CameraRaycast script

--- Assets/Metatravel/Scripts/CameraRaycast.cs
+++ Assets/Metatravel/Scripts/CameraRaycast.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
 
 public class CameraRaycast : MonoBehaviour
 {
@@ -12,22 +13,27 @@ public class CameraRaycast : MonoBehaviour
     [SerializeField] GameObject crossHair;
     public GameObject whichButton;
     [SerializeField] GameObject mainCamera;
+    [SerializeField] PhotonView PV;
 
 
     private void Update()
     {
-        if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward), out raycastHit, raycastLength, layerMask.value))
+        if (PV.IsMine)
         {
-            crossHair.GetComponent<Image>().color = Color.yellow;
-            crossHair.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
-            isHitButton = true;
-            whichButton = raycastHit.transform.gameObject;
+            if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward), out raycastHit, raycastLength, layerMask.value))
+            {
+                crossHair.GetComponent<Image>().color = Color.yellow;
+                crossHair.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+                isHitButton = true;
+                whichButton = raycastHit.transform.gameObject;
+            }
+            else
+            {
+                crossHair.GetComponent<Image>().color = Color.white;
+                crossHair.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+                isHitButton = false;
+            }
         }
-        else
-        {
-            crossHair.GetComponent<Image>().color = Color.white;
-            crossHair.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-            isHitButton = false;
-        }
+        
     }
 }
