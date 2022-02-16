6b8e641a3136fd506eb82f90071486301ce8db15
efetigli <netigli@hotmail.com>
2022-02-16 21:27:21 +0300

    Update PlayerInteraction Script

--- Assets/Metatravel/Player/Input/PlayerInteraction.cs
+++ Assets/Metatravel/Player/Input/PlayerInteraction.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using Photon.Pun;
 
 public class PlayerInteraction : MonoBehaviour
 {
     [SerializeField] GameObject mainCamera;
+    [SerializeField] PhotonView PV;
     private Transform targetPosition;
 
     public bool isChangePosition = false;
@@ -44,7 +46,6 @@ public class PlayerInteraction : MonoBehaviour
                 targetPosition = this.GetComponent<CameraRaycast>().whichButton.transform;
                 rotation = this.GetComponent<CameraRaycast>().whichButton.transform.rotation;
                 cursorManager.GetComponent<CursorHide>().HideAndCenterCursor(false);
-                crossHair.SetActive(false);
                 //computerScreen.SetActive(true);
                 //this.GetComponent<CameraRaycast>().whichButton;
                 this.GetComponent<CameraRaycast>().whichButton.GetComponentInChildren<Canvas>().worldCamera = this.GetComponentInChildren<Camera>();
@@ -55,43 +56,47 @@ public class PlayerInteraction : MonoBehaviour
 
     private void Update()
     {
-        if (isChangePosition)
+        if (PV.IsMine)
         {
-            if (Mathf.Abs(mainCamera.transform.position.y - targetPosition.position.y) < 0.05f)
+            if (isChangePosition)
             {
-                mainCamera.transform.position = targetPosition.position;
-                mainCamera.transform.rotation = Quaternion.Euler(10f, rotation.eulerAngles.y - 180f, rotation.eulerAngles.z);
-                isChangePosition = false;
+                if (Mathf.Abs(mainCamera.transform.position.y - targetPosition.position.y) < 0.05f)
+                {
+                    mainCamera.transform.position = targetPosition.position;
+                    mainCamera.transform.rotation = Quaternion.Euler(10f, rotation.eulerAngles.y - 180f, rotation.eulerAngles.z);
+                    isChangePosition = false;
+                }
+                crossHair.SetActive(false);
+                this.GetComponent<PlayerMenu>().isMenuOpen = true;
+                this.GetComponent<PlayerMenu>().isComputerUIOpen = true;
+                mainCamera.transform.rotation = Quaternion.Slerp(mainCamera.transform.rotation, Quaternion.Euler(10f, rotation.eulerAngles.y - 180f, rotation.eulerAngles.z), turnSpeed * Time.deltaTime);
+                mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, new Vector3(targetPosition.position.x, targetPosition.position.y, targetPosition.position.z), 2 * Time.deltaTime);
+            }
+            //GameObject asd = this.GetComponent<CameraRaycast>().whichButton.gameObject.transform.Find("Exit");
+            if (exit.GetComponent<ButtonExit>().close)
+            {
+                this.GetComponent<PlayerMenu>().isMenuOpen = false;
+                this.GetComponent<PlayerMenu>().isComputerUIOpen = false;
+                this.GetComponent<PlayerInteraction>().isChangePosition = false;
+                cursorManager.GetComponent<CursorHide>().HideAndCenterCursor(true);
+                crossHair.SetActive(true);
+                setCameraToPosition = true;
+                exit.GetComponent<ButtonExit>().SetCloseFalse();
+                this.GetComponent<CameraRaycast>().whichButton.GetComponentInChildren<Canvas>().worldCamera = null;
             }
 
-            this.GetComponent<PlayerMenu>().isMenuOpen = true;
-            this.GetComponent<PlayerMenu>().isComputerUIOpen = true;
-            mainCamera.transform.rotation = Quaternion.Slerp(mainCamera.transform.rotation, Quaternion.Euler(10f, rotation.eulerAngles.y - 180f, rotation.eulerAngles.z), turnSpeed * Time.deltaTime);
-            mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, new Vector3(targetPosition.position.x, targetPosition.position.y, targetPosition.position.z), 2 * Time.deltaTime);
-        }
-        //GameObject asd = this.GetComponent<CameraRaycast>().whichButton.gameObject.transform.Find("Exit");
-        if (exit.GetComponent<ButtonExit>().close)
-        {
-            this.GetComponent<PlayerMenu>().isMenuOpen = false;
-            this.GetComponent<PlayerMenu>().isComputerUIOpen = false;
-            this.GetComponent<PlayerInteraction>().isChangePosition = false;
-            cursorManager.GetComponent<CursorHide>().HideAndCenterCursor(true);
-            crossHair.SetActive(true);
-            setCameraToPosition = true;
-            exit.GetComponent<ButtonExit>().SetCloseFalse();
-            this.GetComponent<CameraRaycast>().whichButton.GetComponentInChildren<Canvas>().worldCamera = null;
-        }
-
-        if (setCameraToPosition)
-        {
-            if (Mathf.Abs(mainCamera.transform.position.y - cameraPosition.transform.position.y) < 0.05f)
+            if (setCameraToPosition)
             {
-                mainCamera.transform.position = cameraPosition.transform.position;
-                setCameraToPosition = false;
-            }
+                if (Mathf.Abs(mainCamera.transform.position.y - cameraPosition.transform.position.y) < 0.05f)
+                {
+                    mainCamera.transform.position = cameraPosition.transform.position;
+                    setCameraToPosition = false;
+                }
 
-            mainCamera.transform.rotation = Quaternion.Slerp(mainCamera.transform.rotation, rotation, turnSpeed * Time.deltaTime);
-            mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, cameraPosition.transform.position, 2 * Time.deltaTime);
+                mainCamera.transform.rotation = Quaternion.Slerp(mainCamera.transform.rotation, rotation, turnSpeed * Time.deltaTime);
+                mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, cameraPosition.transform.position, 2 * Time.deltaTime);
+            }
         }
+        
     }
 }
